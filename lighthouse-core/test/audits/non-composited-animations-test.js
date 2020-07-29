6686d8a5d0a4da09fc45b52c062afdb935206c73
Adam Raine <ad.st.raine@gmail.com>
2020-07-29 17:17:37 -0400

    lint

--- lighthouse-core/test/audits/non-composited-animations-test.js
+++ lighthouse-core/test/audits/non-composited-animations-test.js
@@ -55,7 +55,7 @@ describe('Non-composited animations audit', () => {
       {
         animation: 'beta',
         failureReasons: 'Unsupported CSS Property',
-      }
+      },
     ]);
   });
 });
